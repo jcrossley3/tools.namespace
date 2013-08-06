136a421047a18609528419683aff4d77b372cc69
Jim Crossley <jim@crossleys.org>
2013-08-06 14:14:43 -0400

    Prepping release of tools.namespace using dynapath

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject org.clojars.jcrossley3/tools.namespace "0.2.4-SNAPSHOT"
+  :description "Modified to work anywhere dynapath does"
+  :url "https://github.com/jcrossley3/tools.namespace"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.tcrawley/dynapath "0.2.3"]]
+  :source-paths ("src/main/clojure")
+  :test-paths ("src/test/clojure")
+  :signing {:gpg-key "92439EF5"}
+  :lein-release {:deploy-via :clojars})
